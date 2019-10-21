e0f81338c3b85957339f3420e3a00587f8fe6293
Gerard Dethier <g.dethier@gmail.com>
2019-10-21 19:30:16 +0200

    Add bulk message send.

--- src/main/java/poussecafe/pulsar/PulsarMessageSender.java
+++ src/main/java/poussecafe/pulsar/PulsarMessageSender.java
@@ -1,7 +1,10 @@
 package poussecafe.pulsar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
 import org.apache.pulsar.client.api.Producer;
@@ -87,4 +90,34 @@ public class PulsarMessageSender extends MessageSender {
     }
 
     private Map<String, Producer<String>> producers = new HashMap<>();
+
+    @Override
+    public void sendMarshalledMessages(List<OriginalAndMarshaledMessage> value) {
+        Map<Producer<String>, List<OriginalAndMarshaledMessage>> messagesPerProducer = messagesPerProducer(value);
+        for(Entry<Producer<String>, List<OriginalAndMarshaledMessage>> entry : messagesPerProducer.entrySet()) {
+            sendBulk(entry);
+        }
+    }
+
+    private Map<Producer<String>, List<OriginalAndMarshaledMessage>> messagesPerProducer(List<OriginalAndMarshaledMessage> value) {
+        Map<Producer<String>, List<OriginalAndMarshaledMessage>> messagesPerProducer = new HashMap<>();
+        for(OriginalAndMarshaledMessage message : value) {
+            Producer<String> producer = producer(message.original());
+            List<OriginalAndMarshaledMessage> producerMessages = messagesPerProducer.computeIfAbsent(producer, key -> new ArrayList<>());
+            producerMessages.add(message);
+        }
+        return messagesPerProducer;
+    }
+
+    private void sendBulk(Entry<Producer<String>, List<OriginalAndMarshaledMessage>> entry) {
+        Producer<String> producer = entry.getKey();
+        for(OriginalAndMarshaledMessage message : entry.getValue()) {
+            producer.sendAsync((String) message.marshaled());
+        }
+        try {
+            producer.flush();
+        } catch (PulsarClientException e) {
+            throw new PousseCafeException("Unable to send messages to Pulsar broker", e);
+        }
+    }
 }
